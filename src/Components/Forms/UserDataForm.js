0c695a89ced25523af040144d0691537ba0bf10c
Eric Posas <web_dev@tutanota.com>
2019-04-24 14:42:09 -0500

    added validations

--- src/Components/Forms/UserDataForm.js
+++ src/Components/Forms/UserDataForm.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import '../../scss/user-data-form.scss'
 
+const required = value => value ? undefined : 'Required'
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Invalid email address' : undefined
+
 class UserDataForm extends Component {
   render(){
     const { handleSubmit } = this.props
@@ -9,15 +14,15 @@ class UserDataForm extends Component {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">First&nbsp;&nbsp;</label>
-          <Field name="firstName" component="input" type="text"/>
+          <Field name="firstName" component="input" validate={[required]} type="text"/>
         </div>
         <div>
           <label htmlFor="lastName">Last&nbsp;&nbsp;</label>
-          <Field name="lastName" component="input" type="text"/>
+          <Field name="lastName" component="input" validate={[required]} type="text"/>
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <Field name="email" component="input" type="email"/>
+          <Field name="email" component="input" validate={[required, email]} type="email"/>
         </div>
         <button type="submit">Submit</button>
       </form>
